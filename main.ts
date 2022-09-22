97a3d8ec0df3c36adfcf3ae562c3f90cd58ffd14
CamilaCoelh <114154096+CamilaCoelh@users.noreply.github.com>
2022-09-22 08:50:33 -0300

    apenas o melhor

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+input.onPinPressed(TouchPin.P0, function () {
+    music.playMelody("C E C E D C D C ", 120)
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("JÁ!")
+    music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Forever)
+    basic.pause(randint(0, 5000))
+    basic.showLeds(`
+        . . . . #
+        . . # . #
+        . # # # .
+        . . # . .
+        . # . # .
+        `)
+})
