eb01e6351cf015d4ef492793fdd494a686089051
CamilaCoelh <114154096+CamilaCoelh@users.noreply.github.com>
2022-10-06 08:48:40 -0300

    completamos

--- main.ts
+++ main.ts
@@ -5,12 +5,50 @@ input.onPinPressed(TouchPin.P0, function () {
     basic.showNumber(1)
     basic.showString("JÁ!")
     music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Forever)
-    basic.pause(randint(0, 5000))
+    basic.pause(randint(1000, 5000))
     basic.showLeds(`
         . . . . #
         . . # . #
         . # # # .
-        . . # . .
+        . . # . #
         . # . # .
         `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    if (touch == 0) {
+        cont_2 += 1
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . . # . .
+            . # . . .
+            . # # # .
+            `)
+        basic.pause(1000)
+        basic.clearScreen()
+    }
+})
+input.onPinPressed(TouchPin.P1, function () {
+    if (touch == 0) {
+        cont_1 += 1
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+        basic.pause(1000)
+        basic.clearScreen()
+    }
+})
+let touch = 0
+let cont_2 = 0
+let cont_1 = 0
+cont_1 += 0
+cont_2 += 0
+basic.forever(function () {
+	
 })
